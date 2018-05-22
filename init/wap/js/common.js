372ce418a824bccc131e89a11a30e3494d69d91e
吴帅鹏 <shuaipeng@wushuang.me>
2018-05-22 17:11:01 +0800

    增加slick  省市联动 common  验证  封装方法

--- init/wap/js/common.js
+++ init/wap/js/common.js
@@ -8,6 +8,12 @@ $(function(){
 			$("html").css("font-size",size+"px");
 		}
 	})
+	linkage(config)
+	$("#fullpage").fullpage({
+		'css3' : false,
+		verticalCentered : !1,
+		anchors:["1","2","3","4","5","6"]
+	})
 	Dream.Preload.load(progress, complete);
 })
 /**
@@ -64,4 +70,160 @@ function fakeLoading(obj,reso,speed){
 			clearTimeout(_timer);
 		}
 	},speed/reso);
+}
+
+/**
+ * 弹窗
+ * @param {Object} n
+ * 1 : 车型配置
+ * 2 : 试驾信息
+ */
+function popup(n){
+	$(".popup,.pop").hide();
+	$.fn.fullpage.setAllowScrolling(true);
+	switch(n){
+		case 1:
+			$.fn.fullpage.setAllowScrolling(false);
+			$(".popup,.popup_testDrive").show();
+			break;
+		case 2:
+			$(".popup,.popup_info").show();
+			$.fn.fullpage.setAllowScrolling(false);
+			break;
+	}
+}
+
+//轮播图
+function funcSlick(obj,number,prev,next){
+	obj.attr("data-slick","yes");
+	obj.slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        draggable: false,
+        prevArrow: obj.parent().find(prev||".prev"),
+        nextArrow: obj.parent().find(next||".next"),
+        autoplay:false,
+        slidesToShow : number||1
+    });
+}
+function funcPlayer(vid){
+	//暂停上一个视频
+	if(_player) _player.pause()
+	//加载视频
+	var video = new tvp.VideoInfo();
+	video.setVid(vid);
+	var player =new tvp.Player();
+	player.create({
+		width : "100%",
+		height : "100%",
+		video : video,
+		modId : "player",
+		autoplay : false,
+		pic:"images/section1_video_pic.jpg",
+        flashWmode:"opaque",
+        onwrite:function(){
+        	_player = player	
+        }
+	});
+}
+/*---------------------------验证------------------------------*/
+function verify(){
+	var _this = $(".popup_info")
+	//姓名 
+	var name = _this.find(".name input").val();
+    if (validName(name)){
+    	if(name=="请输入联系人"){
+            alert("请输入联系人");
+            return false;
+        }
+    }else {
+        alert("请输入正确的姓名");
+        return false;
+    }
+//	性别
+	var sex = _this.find(".sex .positive").attr("data-sex");
+	if(!sex){
+		alert("请选择性别");
+		return false;
+	}
+//	手机
+	var phone = _this.find(".phone input").val();
+    if (validPhone(phone)){
+    }else {
+    	if(phone == ""){
+    		alert("请输入手机号码");
+	        return false;
+    	}else{	
+    	    alert("请输入正确的手机号码");
+	        return false;
+    	}
+    }
+//	省份
+	var pro = _this.find(".pro span").text();
+	if(!pro || pro == "请选择省份"){
+		alert("请选择省份");
+        return false;
+	}
+//	城市
+	var city = _this.find(".city span").text();
+	if(!city || city == "请选择城市"){
+		alert("请选择城市");
+        return false;
+	}
+//	经营商
+	var dealer = _this.find(".dealer span").text();
+	if(!dealer || dealer=="请选择经销商"){
+		alert("请选择经销商");
+        return false;
+	}
+//	协议
+	var agreement = !_this.find(".agreement .tick").hasClass("positive");
+	if(!agreement){
+		alert("请勾选协议");
+        return false;
+	}
+	
+    //数据保存
+    var data = {
+    	name	: name,
+    	sex		: sex,
+    	phone	: phone,
+    	pro		: pro,
+    	city	: city,
+    	dealer	: dealer,
+    	agreement: agreement
+    }
+    alert("保存成功")
+    console.log(data)
+    testDriveInit()
+    
+	// 验证姓名的正则表达式
+	function validName(name){
+	    var pattern=/^([a-zA-Z]{1,20}|[\u4e00-\u9fa5]{1,10}|[\u4e00-\u9fa5a-zA-Z]{1,20})$/;
+	    return pattern.test(name)
+	}
+	//手机号的验证  正则表达式
+	function validPhone(phone){
+	    var pattern=/^1[34578][0-9]{9}$/;
+	    return pattern.test(phone)
+	}
+}
+//选择框改变
+function funcSelectChange(obj,obj1,obj2){		
+	obj.on("change", function(){
+		var text = $(this).find("option:selected").text()
+		$(this).prev().text(text)
+		if(obj1) testDriveInit(obj1)
+		if(obj2) testDriveInit(obj2)
+	})
+}
+//验证初始化
+function testDriveInit(number){
+	if(!number) number = "all";
+	if(number == "name" || number == "all" ) $(".popup_info .name input").val("");
+	if(number == "phone" || number == "all" ) $(".popup_info .phone input").val("");
+	if(number == "pro" || number == "all" ) $(".popup_info .pro span").text("")
+	if(number == "city" || number == "all" ) $(".popup_info .city span").text("");
+	if(number == "dealer" || number == "all" ) $(".popup_info .dealer span").text("");
 }
---
+++ init/wap/js/linkage.js
@@ -0,0 +1,120 @@
+/**
+ * 省市联动
+ * @param config
+ * linkage(config);
+ * 2017/8/1
+ */
+
+var where = {"黑龙江":{"双鸭山市":["广汽传祺博大店"],"七台河市":["广汽传祺弘远店"],"佳木斯市":["广汽传祺佳木斯通途店"]},"吉林":{"白山市":["广汽传祺通展店"],"通化市":["广汽传祺胜笛店"],"梅河口市":["广汽传祺梅河口广进店"],"吉林市":["广汽传祺吉林成祺店"],"延吉市":["广汽传祺中荣店"]},"辽宁":{"葫芦岛市":["广汽传祺卓远店"],"朝阳市":["广汽传祺泰骏达店"],"盘锦市":["广汽传祺弘茂店"]},"内蒙古":{"赤峰市":["广汽传祺森航店"]},"河北":{"石家庄市":["广汽传祺祺杰店","广汽传祺兴和店","广汽传祺庞大祺广店"],"张家口市":["广汽传祺路丰汇鑫店"]},"山东":{"淄博市":["广汽传祺淄博轿辰新隆店"]},"安徽":{"黄山市":["广汽传祺骏瑞店"],"六安市":["广汽传祺六安晟鑫店"]},"福建":{"福州市":["广汽传祺福州瑞骐店"]},"江苏":{"连云港市":["广汽传祺连云港俊祺店"],"如皋市":["广汽传祺如皋益昌大生店"],"南京市":["广汽传祺南京双诚店"],"徐州市":["广汽传祺徐州美盈店"],"宿迁市":["广汽传祺沭阳天泓欣泰店"]},"浙江":{"温岭市":["广汽传祺温岭锦华店"],"台州市":["广汽传祺台州海川店"]},"广东":{"四会市":["广汽传祺四会广万利店"],"珠海市":["广汽传祺珠海悦祺店"],"江门市":["广汽传祺江门合祺店"],"汕尾市":["广汽传祺汕尾信德行店"]},"广西":{"桂林市":["广汽传祺桂林祺辰店"]},"海南":{"三亚市":["广汽传祺三亚高祺店"]},"河南":{"洛阳市":["广汽传祺保行店"],"平顶山市":["广汽传祺明行店","广汽传祺明锋店"],"三门峡市":["广汽传祺君奇店"],"驻马店市":["广汽传祺祥瑞店","广汽传祺骏富店"],"焦作市":["广汽传祺焦作优驰店"]},"湖北":{"荆州市":["广汽传祺荆州瑞之泽店"],"武汉市":["广汽传祺鸿康明店"]},"湖南":{"常德市":["广汽传祺天宝店"],"张家界市":["广汽传祺世茂店"],"永州市":["广汽传祺帝旺店"],"株洲市":["广汽传祺株洲安迅店"]},"江西":{"吉安市":["广汽传祺鑫祺店"],"萍乡市":["广汽传祺国力店"],"新余市":["广汽传祺新余广琪店"]},"甘肃":{"张掖市":["广汽传祺华逸店"],"平凉市":["广汽时代红星店"],"天水市":["广汽传祺祺盛达店"],"兰州市":["广汽传祺兰州深港店"]},"宁夏":{"吴忠市":["广汽传祺吴忠恒信店"],"石嘴山市":["广汽传祺石嘴山恒信店"]},"陕西":{"西安市":["广汽传祺西安锦淇店"],"榆林市":["广汽传祺榆林新白云店"],"延安市":["广汽传祺延安汇能店"]},"新疆":{"库尔勒":["广汽传祺海利昕祺店"],"伊犁哈萨克自治州":["广汽传祺奎屯华科店"],"哈密市":["广汽传祺哈密广骏店"]},"贵州":{"毕节市":["广汽传祺佰润正和"]},"四川":{"凉山州市":["广汽传祺凯达店"],"乐山市":["广汽传祺西部店"],"巴中市":[""],"绵阳市":["广汽传祺绵阳泰康店"],"眉山市":["广汽传祺眉山华信恒驰店"],"攀枝花市":["广汽传祺攀枝花长禧店"],"自贡市":["广汽传祺自贡中盛天翔店"],"彭州市":["广汽传祺彭州信祺店"],"雅安市":["广汽传祺雅安中盛晶品店"]},"云南":{"普洱市":["广汽传祺祺鑫店"],"西双版纳傣族自治州":["广汽传祺昌达店","广汽传祺西双版纳昌达店"],"文山市":["广汽传祺骏驰店"],"昆明市":["广汽传祺昆明安飞店"]}}
+var config = {
+    // 'url': '/default/getdata',
+    'data': where,
+    'select': [
+        {'id': 'pro', 'text': '请选择省份', 'value': '请选择省份'},	//省
+        {'id': 'city', 'text': '请选择城市','value': '请选择城市'},			//市
+        {'id': 'dealer', 'text': '请选择经销商','value': '请选择经销商'}				//code
+    ]
+};
+var linkage = function(config,prefix){
+    //设置select默认选项
+    function setDefault(obj, text, value) {
+        obj.options[0] = new Option(text, value);
+    }
+    //设置select选项
+    function setOption(obj, text, value) {
+        obj.options[obj.options.length] = new Option(text, value);
+    }
+    //重置select选项
+    function resetOptions(id) {
+        var obj = document.getElementById(id);
+        while (obj.options.length > 1) {
+            obj.remove(1);
+        }
+    }
+    //获取选中的值
+    function getSelectValue(id) {
+        var obj = document.getElementById(id);
+
+        return obj.options[obj.selectedIndex].value;
+    }
+    //设置onchange事件
+    function setChange(obj, data, i) {
+        addEvent(obj, 'change', function(){
+            //重置本级以后所有的select
+            for (var j = i+1; j < config.select.length; j++) {
+                resetOptions(config.select[j].id);
+            }
+            //如果当前不是默认选项，设置下一级的相应选项
+            var v = config.select[i].value ? config.select[i].value : '';
+            if (this.value != v) {
+                var next_obj = document.getElementById(config.select[i+1].id);
+                var next_data = data;
+                for (var x = 0; x <= i; x++) {
+                    var index = getSelectValue(config.select[x].id);
+                    next_data = next_data[index];
+                }
+                for (var d in next_data) {
+                    //next_data可能是数组也可能是object
+                    var k = next_data.length ? next_data[d] : d;
+                    setOption(next_obj, k, k);
+                }
+            }
+        });
+    }
+    var _addEventArr = new Array();
+    function addEvent(d, c, b) {
+        var a = d;
+        if (typeof(d) == "string") {
+            d = document.getElementById(d)
+        } else {
+            a = d.id
+        }
+        if (_addEventArr[a] == 1) {
+            return
+        } else {
+            _addEventArr[a] = 1
+        }
+        if (d.addEventListener) {
+            d.addEventListener(c, b, false)
+        } else {
+            if (d.attachEvent) {
+                d.attachEvent("on" + c, function() {
+                    return b.apply(d, new Array(window.event))
+                })
+            }
+        }
+    }
+    //初始化
+    function init(data) {
+        for (var i in config.select) {
+//      	config.select[i].id = prefix + config.select[i].id
+            var item = config.select[i];
+            var obj = document.getElementById(item.id);
+            var text = item.text ? item.text : '请选择';
+            var value = item.value ? item.value : '';
+            //设置默认选项
+            setDefault(obj, text, value);
+            //为第一级设置选项
+            if (i == 0) {
+                for (var d in data) {
+                    setOption(obj, d, d);
+                }
+            }
+            //除了最后一级，设置onchange事件
+            if (i < config.select.length - 1) {
+                setChange(obj, data, parseInt(i));
+            }
+        }
+    };
+    if (config.data) {
+        init(config.data);
+    } else if (config.url) {
+        loader('ajax/ajax', function(){
+            $.ajax.getJSON(config.url, function(data){
+                init(data);
+            });
+        });
+    } else {
+        alert('linkage has no data or url');
+    }
+};
