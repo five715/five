79e590735289b8dd28d7ab5ac24c16b53fc4a839
吴帅鹏 <shuaipeng@wushuang.me>
2018-06-08 18:15:49 +0800

    3D颗粒

---
+++ demo/three/granule/js/Granule.class.js
@@ -0,0 +1,574 @@
+var Demo = {};
+Demo.VER = "1.0.0";
+/**
+ *	枚举，类型
+ */
+Demo.kType = {
+	DRAG: 0,
+	GRAVITY: 1
+}
+Demo.Event = {
+	
+}
+/**
+ *	预先加载
+ */
+Demo.Preload = {
+	_queue : null,	//loder
+	_images : [
+		{id:"bg", src:"panorama_bg.jpg"},
+	],
+	_sounds : [
+	
+	],
+	/**
+	 *	初始化
+	 */
+	init : function(){
+		this._queue = new createjs.LoadQueue(true);
+		this._queue.loadManifest(this._images, false, "images/");
+//		this._queue.loadManifest(this._sounds, false, "");
+//		createjs.Sound.registerSounds(this._sounds);
+	},
+	/**
+	 *	加载
+	 */
+	load : function(progress, complete){
+		if(!this._queue) this.init();
+		if(progress)this._queue.on("progress", progress, this);//资源载入中
+		if(complete)this._queue.on("complete", complete, this);//资源载入完毕
+		this._queue.load();
+	},
+	/**
+	 *	获取loader
+	 */
+	getQueue : function(){
+		return this._queue;
+	},
+	/**
+	 *	获取文件实体
+	 */
+	getResult : function(id){
+		return this._queue.getResult(id);
+	}
+};
+
+/**
+ * 主函数
+ */
+Demo.main = function(canvas){
+	var _this = this;
+	var WIDTH = 414,
+		HEIGHT = 736;
+	var __camera = null,	//摄像头
+		__scene = null,		//场景
+		__room = null,
+		__renderer = null;	//渲染器
+	var _controls = null,
+		_controls2 = null;
+	var _raycaster = new THREE.Raycaster(),	//射线
+		_objects = [];	//检测元素
+	var _mouseControls = null;
+	var CAMERA_ORIGIN = new THREE.Vector3(0, 0, 1);
+	var CAMERA = {
+		x : 0,
+		y : 0,
+		z : 0,
+		fov : 50,
+		near : 10,
+		far : 2000
+	}
+	
+	var _angle = 0,
+		_radian = 0
+	var _tween = false;
+	_this.init = function(container){
+		__camera = new THREE.PerspectiveCamera(
+			CAMERA.fov,
+			WIDTH/HEIGHT,
+			CAMERA.near,
+			CAMERA.far)
+		__camera.position.copy(CAMERA_ORIGIN);
+		
+		__room = new THREE.Object3D();
+		__scene = new THREE.Scene();
+		
+		__scene.add(__room);
+		__renderer = new THREE.WebGLRenderer({
+			canvas: container,
+			antialias: true,
+			alpha:false
+		});
+		__renderer.setPixelRatio(window.devicePixelRatio);
+		__renderer.setSize(WIDTH,HEIGHT);
+		var light = new THREE.AmbientLight( 0xffffff , 0.8);
+		__scene.add(light);
+		lightInit();
+//		_this.setView();
+		_this.controls();
+//		_this.control();
+		animate();		
+	}
+	_this.launch = function(){
+//		for(var i = 30 ; i > 0 ; i --){
+//			__scene.add(setPos(i--,true,20));
+//			__scene.add(setPos(i,false,20));
+//		}
+		var i = 300;
+		var t = setInterval(function(){
+			var geometry = new THREE.SphereGeometry( 1, 30, 30 );
+			var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+			var sphere = new THREE.Mesh( geometry, material );
+			sphere.name = sphere.id = "g"+i;
+        	sphere.position.z= -100;
+        	
+        	var radian = i*10//THREE.Math.degToRad(i*3);
+        	var radian1 = THREE.Math.degToRad(i);
+        	var r = 100;
+        	console.log(radian);
+        	new TWEEN.Tween(sphere.position)
+        		.to({
+//      			x:100*Math.sin(radian),
+//      			z:100*Math.cos(radian),
+//      			y:20*Math.sin(-radian+1)*Math.cos(-radian+1)
+        			x:r*Math.sin(radian)*Math.cos(i),
+					y:r*Math.sin(radian)*Math.sin(i),
+					z:r*Math.cos(radian)
+        		},1000)
+        		.repeat(1)
+        		.onComplete(a)
+        		.start();
+        	__scene.add(sphere);
+			i--
+			if(i<0) clearInterval(t)
+		},5)
+	}
+	function a (){
+		if(_tween) return;
+		_tween = true;
+//		_this.control();
+	}
+	//假贝塞尔
+	function setPos(i,bol,r){
+		var geometry = new THREE.SphereGeometry( 1, 30, 30 );
+		var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+		var sphere = new THREE.Mesh( geometry, material );
+		sphere.name = sphere.id = "l"+i;
+        var radian = THREE.Math.degToRad(i*6);
+        var fluctuate = 4
+        sphere.position.x = 100*Math.sin(bol?-radian:radian);
+        sphere.position.z = 100*Math.cos(radian);
+        sphere.position.y = r*Math.sin(-radian*fluctuate);
+        return sphere;
+	}
+	/**
+	 * 设置背景
+	 */
+	_this.setView = function(image){
+		var view = new Demo.View(image);
+		__room.add(view);
+	}
+	_this.controls = function(){
+		_mouseControls = new THREE.TrackballControls(__camera);
+	}
+	/**
+	 * 控制
+	 */
+	_this.control = function(type, limit){
+		if(type = Demo.kType.GRAVITY){
+			_controls = new THREE.DeviceOrientationControls(__camera);	//加载陀螺仪
+			_controls2 = new THREE.rotaControls(__room, __renderer.domElement);
+		}else{
+			_controls = new THREE.AntiMeshControls(__camera, __renderer.domElement, limit);
+		}
+		__renderer.domElement.addEventListener( 'click', onClick, false );
+		__renderer.domElement.addEventListener( 'touchstart', onTouchStart, false );
+	}
+	/**
+	 *	点击
+	 */
+	function onClick( e ) {
+		e.preventDefault();
+		hitTest( e.clientX, e.clientY );
+	}
+	function onTouchStart (e) {
+		e.preventDefault();
+		hitTest( e.touches[0].clientX, e.touches[0].clientY );
+	}
+	function hitTest(){
+		
+	}
+	function lightInit(){
+		dirLight = new THREE.DirectionalLight(0xfecd80,0.7);
+		dirLight.position.set(-163,241,420);
+		__scene.add(dirLight);
+	}
+	function animate(){
+		requestAnimationFrame(animate);
+		if(_mouseControls) _mouseControls.update();
+		if(_controls)_controls.update();
+		if(_controls2)_controls2.update();
+		TWEEN.update();
+		__renderer.clear();
+		__renderer.render( __scene, __camera );
+	}
+	_this.init(canvas);
+}
+Object.assign( Demo.main.prototype, THREE.EventDispatcher.prototype);
+Demo.main.prototype.constructor = Demo.main;
+
+
+
+/**
+ * 全景
+ */
+Demo.View = function(image){
+	var _this = this;
+	var RADIUS = 100,
+		SEGMENTS = 60;
+	_this.init = function (image){
+		THREE.Mesh.call(_this);
+		var texture = new THREE.Texture(image);
+		texture.needsUpdate = true;
+		var geometry = new THREE.SphereGeometry(RADIUS, SEGMENTS, SEGMENTS);
+		
+		var material = new THREE.MeshBasicMaterial({map: texture, side: THREE.BackSide});
+		
+		_this.name = "view";
+		_this.geometry = geometry;
+		_this.material = material;
+	}
+	_this.init(image)
+}
+Demo.View.prototype = Object.create( THREE.Mesh.prototype );
+Demo.View.prototype.constructor = Demo.View;
+
+/*-------------------------------------------------------*/
+
+/**
+ *	@desc	运动物体
+ *	@author minamoto
+ *	@param	object	控制的物体
+ *	@param	domElement	应用事件的承载
+ *	@param	limit	限制上限位置
+ */
+
+THREE.AntiMeshControls = function ( object, domElement, limit ) {
+
+	var _this = this;
+	
+	var TOUCH_UNIT = 0.2;
+	
+	var _touchParam = {};
+	var _lon = -90;
+	var _lat = 0;
+	var _limit = limit || 85;
+
+	this.object = object;
+	this.domElement = ( domElement !== undefined ) ? domElement : document;
+	
+	var _movePrev = new THREE.Vector2();
+	var _moveCurr = new THREE.Vector2();
+
+	this.update = function () {
+		_lat = Math.max( - _limit, Math.min( _limit, _lat ) );
+		var phi = THREE.Math.degToRad( 90 - _lat );
+		var theta = THREE.Math.degToRad( _lon );
+
+		var target = new THREE.Vector3();
+		target.x = Math.sin( phi ) * Math.cos( theta );
+		target.y = Math.cos( phi );
+		target.z = Math.sin( phi ) * Math.sin( theta );
+		//this.object.rotation.y = -theta;
+		this.object.lookAt( target );
+	};
+
+	this.reset = function () {
+
+	};
+	function touchstart ( e ) {
+		e.preventDefault();
+		var touch = e.touches[ 0 ];
+		_touchParam.x = touch.screenX;
+		_touchParam.y = touch.screenY;
+	}
+	function touchend( e ) {
+		e.preventDefault();
+	}
+	function touchmove ( e ) {
+		e.preventDefault();
+		var touch = e.touches[ 0 ];
+		_lon -= ( touch.screenX - _touchParam.x ) * TOUCH_UNIT;
+		_lat += ( touch.screenY - _touchParam.y ) * TOUCH_UNIT;
+		_touchParam.x = touch.screenX;
+		_touchParam.y = touch.screenY;
+	}
+	function mousewheel ( event ) {
+		console.log(event);
+	}
+	function mousedown ( event ) {
+		event.preventDefault();
+		event.stopPropagation();
+		document.addEventListener( 'mousemove', mousemove, false );
+		document.addEventListener( 'mouseup', mouseup, false );
+	}
+	function mousemove ( event ) {
+		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+		_lon -= movementX * TOUCH_UNIT;
+		_lat += movementY * TOUCH_UNIT;
+	}
+	function mouseup ( event ) {
+		document.removeEventListener( 'mousemove', mousemove );
+		document.removeEventListener( 'mouseup', mouseup );
+	}
+	function contextmenu( event ) {
+		event.preventDefault();
+	}
+	
+	this.dispose = function() {
+		this.domElement.removeEventListener( 'contextmenu', contextmenu, false );
+		this.domElement.removeEventListener( 'mousedown', mousedown, false );
+		this.domElement.removeEventListener( 'mousewheel', mousewheel, false );
+
+		this.domElement.removeEventListener( 'touchstart', touchstart, false );
+		this.domElement.removeEventListener( 'touchend', touchend, false );
+		this.domElement.removeEventListener( 'touchmove', touchmove, false );
+
+		document.removeEventListener( 'mousemove', mousemove, false );
+		document.removeEventListener( 'mouseup', mouseup, false );
+
+	};
+	this.domElement.addEventListener( 'contextmenu', contextmenu, false );
+	this.domElement.addEventListener( 'mousedown', mousedown, false );
+	this.domElement.addEventListener( 'mousewheel', mousewheel, false );
+
+	this.domElement.addEventListener( 'touchstart', touchstart, false );
+	this.domElement.addEventListener( 'touchend', touchend, false );
+	this.domElement.addEventListener( 'touchmove', touchmove, false );
+	
+	this.update();
+
+};
+
+THREE.AntiMeshControls.prototype = Object.create( THREE.EventDispatcher.prototype );
+THREE.AntiMeshControls.prototype.constructor = THREE.AntiMeshControls;
+
+/**
+ * @author richt / http://richt.me
+ * @author WestLangley / http://github.com/WestLangley
+ *
+ * W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
+ */
+
+THREE.DeviceOrientationControls = function( object ) {
+
+	var scope = this;
+
+	this.object = object;
+	this.object.rotation.reorder( "YXZ" );
+
+	this.enabled = true;
+
+	this.deviceOrientation = {};
+	this.screenOrientation = 0;
+
+	this.alpha = 0;
+	this.alphaOffsetAngle = 0;
+
+
+	var onDeviceOrientationChangeEvent = function( event ) {
+
+		scope.deviceOrientation = event;
+
+	};
+
+	var onScreenOrientationChangeEvent = function() {
+
+		scope.screenOrientation = window.orientation || 0;
+
+	};
+
+	// The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
+
+	var setObjectQuaternion = function() {
+
+		var zee = new THREE.Vector3( 0, 0, 1 );
+
+		var euler = new THREE.Euler();
+
+		var q0 = new THREE.Quaternion();
+
+		var q1 = new THREE.Quaternion( - Math.sqrt( 0.5 ), 0, 0, Math.sqrt( 0.5 ) ); // - PI/2 around the x-axis
+
+		return function( quaternion, alpha, beta, gamma, orient ) {
+
+			euler.set( beta, alpha, - gamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
+
+			quaternion.setFromEuler( euler ); // orient the device
+
+			quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
+
+			quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) ); // adjust for screen orientation
+
+		}
+
+	}();
+
+	this.connect = function() {
+
+		onScreenOrientationChangeEvent(); // run once on load
+
+		window.addEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
+		window.addEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+
+		scope.enabled = true;
+
+	};
+
+	this.disconnect = function() {
+
+		window.removeEventListener( 'orientationchange', onScreenOrientationChangeEvent, false );
+		window.removeEventListener( 'deviceorientation', onDeviceOrientationChangeEvent, false );
+
+		scope.enabled = false;
+
+	};
+
+	this.update = function() {
+
+		if ( scope.enabled === false ) return;
+
+		var alpha = scope.deviceOrientation.alpha ? THREE.Math.degToRad( scope.deviceOrientation.alpha ) + this.alphaOffsetAngle : 0; // Z
+		var beta = scope.deviceOrientation.beta ? THREE.Math.degToRad( scope.deviceOrientation.beta ) : 0; // X'
+		var gamma = scope.deviceOrientation.gamma ? THREE.Math.degToRad( scope.deviceOrientation.gamma ) : 0; // Y''
+		var orient = scope.screenOrientation ? THREE.Math.degToRad( scope.screenOrientation ) : 0; // O
+
+		setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
+		this.alpha = alpha;
+
+	};
+
+	this.updateAlphaOffsetAngle = function( angle ) {
+
+		this.alphaOffsetAngle = angle;
+		this.update();
+
+	};
+
+	this.dispose = function() {
+
+		this.disconnect();
+
+	};
+
+	this.connect();
+
+};
+/**
+ *	@desc	运动物体
+ *	@author minamoto
+ *	@param	object	控制的物体
+ *	@param	domElement	应用事件的承载
+ *	@param	limit	限制上限位置
+ */
+
+THREE.rotaControls = function ( object, domElement ) {
+
+	var _this = this;
+	
+	var TOUCH_UNIT = -0.2;
+	
+	var _touchParam = {};
+	var _lon = 0;
+	var _lat = 0;
+
+	this.object = object;
+	this.domElement = ( domElement !== undefined ) ? domElement : document;
+	
+	var _movePrev = new THREE.Vector2();
+	var _moveCurr = new THREE.Vector2();
+
+	this.update = function () {
+		var phi = THREE.Math.degToRad( 90 );
+		var theta = THREE.Math.degToRad( _lon );
+
+		var target = new THREE.Vector3();
+		target.x = Math.sin( phi ) * Math.cos( theta );
+		//target.y = Math.cos( phi );
+		target.z = Math.sin( phi ) * Math.sin( theta );
+		//this.object.rotation.y = -theta;
+		this.object.lookAt( target );
+	};
+
+	this.reset = function () {
+
+	};
+	function touchstart ( e ) {
+		e.preventDefault();
+		var touch = e.touches[ 0 ];
+		_touchParam.x = touch.screenX;
+		_touchParam.y = touch.screenY;
+	}
+	function touchend( e ) {
+		e.preventDefault();
+	}
+	function touchmove ( e ) {
+		e.preventDefault();
+		var touch = e.touches[ 0 ];
+		_lon -= ( touch.screenX - _touchParam.x ) * TOUCH_UNIT;
+		_lat += ( touch.screenY - _touchParam.y ) * TOUCH_UNIT;
+		_touchParam.x = touch.screenX;
+		_touchParam.y = touch.screenY;
+	}
+	function mousewheel ( event ) {
+		console.log(event);
+	}
+	function mousedown ( event ) {
+		event.preventDefault();
+		event.stopPropagation();
+		document.addEventListener( 'mousemove', mousemove, false );
+		document.addEventListener( 'mouseup', mouseup, false );
+	}
+	function mousemove ( event ) {
+		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+		_lon -= movementX * TOUCH_UNIT;
+		_lat += movementY * TOUCH_UNIT;
+	}
+	function mouseup ( event ) {
+		document.removeEventListener( 'mousemove', mousemove );
+		document.removeEventListener( 'mouseup', mouseup );
+	}
+	function contextmenu( event ) {
+		event.preventDefault();
+	}
+	
+	this.dispose = function() {
+		this.domElement.removeEventListener( 'contextmenu', contextmenu, false );
+		this.domElement.removeEventListener( 'mousedown', mousedown, false );
+		this.domElement.removeEventListener( 'mousewheel', mousewheel, false );
+
+		this.domElement.removeEventListener( 'touchstart', touchstart, false );
+		this.domElement.removeEventListener( 'touchend', touchend, false );
+		this.domElement.removeEventListener( 'touchmove', touchmove, false );
+
+		document.removeEventListener( 'mousemove', mousemove, false );
+		document.removeEventListener( 'mouseup', mouseup, false );
+
+	};
+	this.domElement.addEventListener( 'contextmenu', contextmenu, false );
+	this.domElement.addEventListener( 'mousedown', mousedown, false );
+	this.domElement.addEventListener( 'mousewheel', mousewheel, false );
+
+	this.domElement.addEventListener( 'touchstart', touchstart, false );
+	this.domElement.addEventListener( 'touchend', touchend, false );
+	this.domElement.addEventListener( 'touchmove', touchmove, false );
+	
+	this.update();
+
+};
+
+THREE.rotaControls.prototype = Object.create( THREE.EventDispatcher.prototype );
+THREE.rotaControls.prototype.constructor = THREE.rotaControls;
+
---
+++ demo/three/granule/js/common.js
@@ -0,0 +1,228 @@
+var _game = null;
+
+$(function(){
+	$(window).bind("resize load", function(){
+		w = $(".box").width();
+		if(w < 1000){			
+			size = w / 64;
+			$("html").css("font-size",size+"px");
+		}
+	})
+	Demo.Preload.load(progress, complete);
+})
+/**
+ * 加载中
+ */
+function progress(e){
+	var per = Math.floor(e.loaded*100);
+	console.log(per);
+}
+/**
+ * 加载完成
+ */
+function complete(e){
+	onGameStart()
+}
+/**
+ * 游戏开始
+ */
+function onGameStart(){
+	_game = new Demo.main($("#Granule")[0]);
+	_game.launch();
+}
+/**
+ * 游戏结束
+ */
+function onGameOver(){
+	
+}
+/**
+ * 文字dot
+ */
+function animationDot(obj,text,speed){
+	clearTimeout(_timer);
+	var DOT = [".","..","..."];
+	var i = 0;
+	_timer = setInterval(function(){
+		obj.text(text+DOT[i]);
+		i++;
+		if(i >= DOT.length) i = 0;
+	},speed);
+}
+
+/**
+ * 假loading
+ */
+function fakeLoading(obj,reso,speed){
+	var i = 0
+	_timer = setInterval(function(){
+		i+= 100 / reso;
+		pre = i;
+		obj.text(pre+"%...");
+		i++;
+		if(pre >=100){		
+			obj.text("100%...");
+			clearTimeout(_timer);
+		}
+	},speed/reso);
+}
+
+/**
+ * 弹窗
+ * @param {Object} n
+ * 1 : 车型配置
+ * 2 : 试驾信息
+ */
+function popup(n){
+	$(".popup,.pop").hide();
+	$.fn.fullpage.setAllowScrolling(true);
+	switch(n){
+		case 1:
+			$.fn.fullpage.setAllowScrolling(false);
+			$(".popup,.popup_testDrive").show();
+			break;
+		case 2:
+			$(".popup,.popup_info").show();
+			$.fn.fullpage.setAllowScrolling(false);
+			break;
+	}
+}
+
+//轮播图
+function funcSlick(obj,number,prev,next){
+	obj.attr("data-slick","yes");
+	obj.slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        draggable: false,
+        prevArrow: obj.parent().find(prev||".prev"),
+        nextArrow: obj.parent().find(next||".next"),
+        autoplay:false,
+        slidesToShow : number||1
+    });
+}
+function funcPlayer(vid){
+	//暂停上一个视频
+	if(_player) _player.pause()
+	//加载视频
+	var video = new tvp.VideoInfo();
+	video.setVid(vid);
+	var player =new tvp.Player();
+	player.create({
+		width : "100%",
+		height : "100%",
+		video : video,
+		modId : "player",
+		autoplay : false,
+		pic:"images/section1_video_pic.jpg",
+        flashWmode:"opaque",
+        onwrite:function(){
+        	_player = player	
+        }
+	});
+}
+/*---------------------------验证------------------------------*/
+function verify(){
+	var _this = $(".popup_info")
+	//姓名 
+	var name = _this.find(".name input").val();
+    if (validName(name)){
+    	if(name=="请输入联系人"){
+            alert("请输入联系人");
+            return false;
+        }
+    }else {
+        alert("请输入正确的姓名");
+        return false;
+    }
+//	性别
+	var sex = _this.find(".sex .positive").attr("data-sex");
+	if(!sex){
+		alert("请选择性别");
+		return false;
+	}
+//	手机
+	var phone = _this.find(".phone input").val();
+    if (validPhone(phone)){
+    }else {
+    	if(phone == ""){
+    		alert("请输入手机号码");
+	        return false;
+    	}else{	
+    	    alert("请输入正确的手机号码");
+	        return false;
+    	}
+    }
+//	省份
+	var pro = _this.find(".pro span").text();
+	if(!pro || pro == "请选择省份"){
+		alert("请选择省份");
+        return false;
+	}
+//	城市
+	var city = _this.find(".city span").text();
+	if(!city || city == "请选择城市"){
+		alert("请选择城市");
+        return false;
+	}
+//	经营商
+	var dealer = _this.find(".dealer span").text();
+	if(!dealer || dealer=="请选择经销商"){
+		alert("请选择经销商");
+        return false;
+	}
+//	协议
+	var agreement = !_this.find(".agreement .tick").hasClass("positive");
+	if(!agreement){
+		alert("请勾选协议");
+        return false;
+	}
+	
+    //数据保存
+    var data = {
+    	name	: name,
+    	sex		: sex,
+    	phone	: phone,
+    	pro		: pro,
+    	city	: city,
+    	dealer	: dealer,
+    	agreement: agreement
+    }
+    alert("保存成功")
+    console.log(data)
+    testDriveInit()
+    
+	// 验证姓名的正则表达式
+	function validName(name){
+	    var pattern=/^([a-zA-Z]{1,20}|[\u4e00-\u9fa5]{1,10}|[\u4e00-\u9fa5a-zA-Z]{1,20})$/;
+	    return pattern.test(name)
+	}
+	//手机号的验证  正则表达式
+	function validPhone(phone){
+	    var pattern=/^1[34578][0-9]{9}$/;
+	    return pattern.test(phone)
+	}
+}
+//选择框改变
+function funcSelectChange(obj,obj1,obj2){		
+	obj.on("change", function(){
+		var text = $(this).find("option:selected").text()
+		$(this).prev().text(text)
+		if(obj1) testDriveInit(obj1)
+		if(obj2) testDriveInit(obj2)
+	})
+}
+//验证初始化
+function testDriveInit(number){
+	if(!number) number = "all";
+	if(number == "name" || number == "all" ) $(".popup_info .name input").val("");
+	if(number == "phone" || number == "all" ) $(".popup_info .phone input").val("");
+	if(number == "pro" || number == "all" ) $(".popup_info .pro span").text("")
+	if(number == "city" || number == "all" ) $(".popup_info .city span").text("");
+	if(number == "dealer" || number == "all" ) $(".popup_info .dealer span").text("");
+	if(number == "config" || number == "all" ) {
+		$(".testDrive .selectInit").empty();
+		linkage(config)
+	}
+}
