2166a39c7eaa648e004e4781c3dbfc1cd100b60d
吴帅鹏 <shuaipeng@wushuang.me>
2018-12-21 17:35:04 +0800

    mpvue 初始化

---
+++ demo/mpvue/mpvue_init/src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    {{motto}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello World'
+    }
+  }
+}
+</script>
+<style>
+
+</style>
